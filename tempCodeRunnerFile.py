bcc10a56c8fa3afbf691cc74ab5c101cc637ffb4
Alinealc <aline.siqueira@aluno.senac.rio>
2025-12-04 19:37:37 -0300

    Iniciando

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+list_meses = ["janeiro", "fevereiro","março","abril","maio","junho","julho"]
+print(list_meses[0]) #primeiro
+print(list_meses[6])
+print(list_meses[-1]) #último
+print(list_meses[-2]) #penúltimo
+print(list_meses[1:4]) #imprime a partir do índice 1 até o 4-1 (não vai até o quarto)
+print(list_meses[2:]) #imprime a partir do índice 2 até o final
+print(list_meses[:5]) #imprime a partir do índice 0 até o 5-1 
